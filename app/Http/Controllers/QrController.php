37ee6c8962c1ae4ae419caeea98bf862e7355bd1
kishan dudhatra <kishan.dudhatra@multidots.com>
2019-02-01 18:02:16 +0530

    get tweet completed but designed pending.

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-
+use Thujohn\Twitter\Facades\Twitter;
 use Config;
 
 class QrController extends Controller {
@@ -21,35 +21,56 @@ class QrController extends Controller {
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index() {
-        
+
         $random_url = $this->words_url();
-        
+
         return view('welcome', compact('random_url'));
     }
-    
+
     public function generate_sentence() {
         $sentence = '';
-        foreach (config('constants.sentence_formation') as $k =>  $word_key) {
+        foreach (config('constants.sentence_formation') as $k => $word_key) {
             $word_array = config("constants.words.$word_key");
-            if(is_array($word_array)) {
+            if (is_array($word_array)) {
                 $sentence .= $word_array[array_rand($word_array)];
             }
         }
         return $sentence;
     }
-    
+
     public function generate_tweet_url() {
-        return config('constants.twitter.endpoint').'?'.config('constants.twitter.text').'=';
+        return config('constants.twitter.endpoint') . '?' . config('constants.twitter.text') . '=';
     }
-    
+
     public function words_url() {
         $text = $this->generate_sentence();
-        $url = $this->generate_tweet_url(). $text;
+        $url = $this->generate_tweet_url() . $text;
         return $url;
     }
-    
+
     public function tweet() {
         echo $this->words_url();
+    }
+
+    public function getTweet() {
+
+        //$tweets = Twitter::getSearch(['q'=>'#laravelAhm']);
+        /* $tweets = getMentionsTimeline(['count' => 20, 'format' => 'json']);
+          print_r($tweets); */
+
+        try {
+            $response = Twitter::getSearch(['count' => 10, 'q' => '#Laravel', 'result_type' => 'recent']);
+            //$response1 = Twitter::getFavorites(['count' => 10, 'format' => 'array']);
+        } catch (Exception $e) {
+            // dd(Twitter::error());
+            dd(Twitter::logs());
+        }
+        //echo $response->statuses[0]['text'];
+        //$array =  (array) $response;
+        //$array1 =  (array) $response->statuses;
+        //echo '<pre>';print_R($array1);
+        //echo $array['statuses'][0]['text']; 
+        dd($response);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', 'QrController@index');
 Route::get('/words', 'QrController@generate_sentence');
 Route::get('/tweet', 'QrController@tweet');
 Route::get('/words-url', 'QrController@words_url');
-
-
+Route::get('/get-tweet','QrController@getTweet');
+/*Route::get('/get-tweet', function()
+{
+    return Twitter::getSearch(['q'=>'#laravelAhm']);
+});*/
 App::singleton('App\Tweet\Qr',function () {
     return new \App\Tweet\Qr(config(''));
 });
